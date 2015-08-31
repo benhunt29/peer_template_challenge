ae6a34d08ac80ba0bc59f4f87a12ef60c49544db
benhunt29 <benhunt29@gmail.com>
2015-08-31 17:13:04 -0500

    added messages route

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var memes = require('./routes/memes');
+var messages = require('./routes/messages');
 
 
 var app = express();
@@ -25,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/memes', memes);
+app.use('/messages',messages);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/messages.js
@@ -0,0 +1,37 @@
+var memes = require('../models/images.json');
+var messages = require('../models/messages.json');
+var express = require('express');
+var router = express.Router();
+var fs = require('fs');
+var path = require('path');
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+
+/* GET users listing. */
+router.post('/', function(req, res, next) {
+    var id = req.body.id;
+    var message = req.body.message;
+    var file = path.join(__dirname,'../models/messages.json');
+    fs.write(file,)
+    //console.log(req.body);
+    //var file1 = path.join(__dirname,'../models/images.json');
+    //var file2 = path.join(__dirname,'../models/messages.json');
+    //
+    //var memesArray = [];
+    //for (var i = 0;i<5;i++){
+    //    memes[i].message = messages[i].message;
+    //    console.log(memes[i].message, messages[i].message);
+    //    memesArray[i] = memes[i];
+    //}
+    //
+    //
+    ////}
+    //
+    //console.log(memes);
+    //res.render('memes.jade',{"memes": memesArray});
+    res.send(id + " " + message);
+});
+
+
+
+module.exports = router;
