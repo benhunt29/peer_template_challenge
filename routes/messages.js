8f4b4c3a1e2bc06eb1bd3cb4d4368f8487102fdb
benhunt29 <benhunt29@gmail.com>
2015-09-01 08:57:04 -0500

    append messages.json with each message POST

--- routes/messages.js
+++ routes/messages.js
@@ -5,6 +5,7 @@ var router = express.Router();
 var fs = require('fs');
 var path = require('path');
 var bodyParser = require('body-parser');
+var app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 
 /* GET users listing. */
@@ -12,7 +13,30 @@ router.post('/', function(req, res, next) {
     var id = req.body.id;
     var message = req.body.message;
     var file = path.join(__dirname,'../models/messages.json');
-    fs.write(file,)
+
+    fs.readFile(file,'utf8',function(err,data){
+        if (err){
+            next(err);
+        }else{
+            var array = JSON.parse(data);
+            var messages = array;
+            messages.push({"messages":message,
+            "id":id});
+            messages = JSON.stringify(messages);
+
+            fs.writeFile(file,messages,'utf8',function(err){
+                if(err) {
+                    return console.log(err);
+                    next(err);
+                }
+                else{
+                    console.log("Wrote Data!");
+                }
+            });
+        }
+
+    });
+
     //console.log(req.body);
     //var file1 = path.join(__dirname,'../models/images.json');
     //var file2 = path.join(__dirname,'../models/messages.json');
